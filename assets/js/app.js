0bf7f22a572a389fc6ebf6f315ea721900294dae
Antonio Zanon <antonio.zanon99@gmail.com>
2025-04-10 00:49:33 -0300

    Estrutura da página de Início do jogo feita. Agora, trabalhar na estilização

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,34 @@
+document.querySelectorAll('.btn-increment').forEach(btn => {
+    btn.addEventListener('click', function () {
+      const input = this.previousElementSibling; 
+      let valor = parseInt(input.value);
+      let max = parseInt(input.max);
+  
+      if (valor < max) {
+        valor++;
+        input.value = valor;
+        console.log(valor);
+      }
+    });
+  });
+
+
+document.querySelectorAll('.btn-decrement').forEach(btn => {
+    btn.addEventListener('click', function () {
+      const input = this.nextElementSibling;
+      let valor = parseInt(input.value);
+      let min = parseInt(input.min);
+  
+      if (valor > min) {
+        input.value = valor - 1;
+        console.log(valor);
+      }
+    });
+  });
+
+
 function gerarNumeroAleatorio(min,max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
  }
 
- function iniciarJogo(){
-    
- }
+
